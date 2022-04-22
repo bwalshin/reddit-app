707c7b66521a3348421a7bc89890c475ec7ca497
bwalshin <brandon.walshin@gmail.com>
2022-04-21 22:53:03 -0700

    - Update Post layout - Update Header layout - Update Comments visuals - Update component interactions on smaller screens

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -94,58 +94,67 @@ const Post = (props) => {
     <article key={post.id}>
       <Card>
         <div className="post-wrapper">
-          <div className="post-votes-container">
-            <button
-              type="button"
-              className={`icon-action-button up-vote ${
-                voteValue === 1 && 'active'
-              }`}
-              onClick={() => onHandleVote(1)}
-              aria-label="Up vote"
-            >
-              {renderUpVote()}
-            </button>
-            <p className={`post-votes-value ${getVoteType()}`}>
-              {shortenNumber(post.ups, 1)}
-            </p>
-            <button
-              type="button"
-              className={`icon-action-button down-vote ${
-                voteValue === -1 && 'active'
-              }`}
-              onClick={() => onHandleVote(-1)}
-              aria-label="Down vote"
-            >
-              {renderDownVote()}
-            </button>
-          </div>
+
           <div className="post-container">
-            <h3 className="post-title">{post.title}</h3>
 
             <div className="post-image-container">
               <img src={post.url} alt="" className="post-image" />
             </div>
 
-            <div className="post-details">
-              <span className="author-details">
-                <Avatar name={post.author} />
-                <span className="author-username">{post.author}</span>
-              </span>
-              <span>{moment.unix(post.created_utc).fromNow()}</span>
-              <span className="post-comments-container">
+            <div className="post-subcontainer">
+              <div className="post-votes-container">
                 <button
                   type="button"
-                  className={`icon-action-button ${
-                    post.showingComments && 'showing-comments'
+                  className={`icon-action-button up-vote ${
+                    voteValue === 1 && 'active'
                   }`}
-                  onClick={() => onToggleComments(post.permalink)}
-                  aria-label="Show comments"
+                  onClick={() => onHandleVote(1)}
+                  aria-label="Up vote"
                 >
-                  <TiMessage className="icon-action" />
+                  {renderUpVote()}
                 </button>
-                {shortenNumber(post.num_comments, 1)}
-              </span>
+
+                <p className={`post-votes-value ${getVoteType()}`}>
+                  {shortenNumber(post.ups, 1)}
+                </p>
+
+                <button
+                  type="button"
+                  className={`icon-action-button down-vote ${
+                    voteValue === -1 && 'active'
+                  }`}
+                  onClick={() => onHandleVote(-1)}
+                  aria-label="Down vote"
+                >
+                  {renderDownVote()}
+                </button>
+              </div>
+
+              <h3 className="post-title">{post.title}</h3>
             </div>
+
+              <div className="post-details">
+                <span className="author-details">
+                  <Avatar name={post.author} />
+                  <span className="author-username">{post.author}</span>
+                </span>
+                <span>{moment.unix(post.created_utc).fromNow()}</span>
+                <span className="post-comments-container">
+                  <button
+                    type="button"
+                    className={`icon-action-button ${
+                      post.showingComments && 'showing-comments'
+                    }`}
+                    onClick={() => onToggleComments(post.permalink)}
+                    aria-label="Show comments"
+                  >
+                    <TiMessage className="icon-action" />
+                  </button>
+                  {shortenNumber(post.num_comments, 1)}
+                </span>
+              </div>
+  
+
 
             {renderComments()}
           </div>
